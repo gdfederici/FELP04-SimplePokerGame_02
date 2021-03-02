f68553a2852b0c20185d0627dd3d61402d112577
gdfederici <gdfederici@gmail.com>
2021-03-02 10:34:14 +0100

    Order the player's hand.

--- scripts/pokergame.js
+++ scripts/pokergame.js
@@ -1,35 +1,39 @@
 
-var deck = [];
+var playDeck = [];
 var playerHand = [];
+var allPlayersHands = [];
 var players = 4;
 
 // IT- Inizializzo il mazzo inserendo le carte da Asso a Re con 4 cicli, uno per ogni seme.
 // EN- Create the deck by inserting cards A -> King with 4 cycles, one for each suit.
 // Asso/Ace = 14, Goobo/Jack = 11, Donna/Queen = 12, Re/King = 13
 // Cuori/Hearts = 4, Quadri/Diamonds = 3, Fiori/Clubs = 2, Picche/Spades = 1 
-function createDeck() { 
+function createDeck() {
+    let deck = []; 
     for (i=2; i<=14; i++) {
-        deck.push({"card" : i, "suit" : 4});        
+        deck.push({"card" : i, "suit" : "H"});        
     }
     for (i=2; i<=14; i++) {
-        deck.push({"card" : i, "suit" : 3});        
+        deck.push({"card" : i, "suit" : "D"});        
     }
     for (i=2; i<=14; i++) {
-        deck.push({"card" : i, "suit" : 2});        
+        deck.push({"card" : i, "suit" : "C"});        
     }
     for (i=2; i<=14; i++) {
-        deck.push({"card" : i, "suit" : 1});        
+        deck.push({"card" : i, "suit" : "S"});        
     }
+    return deck;
 }
 
 // IT- Creo la mano del giocatore.
 // EN- Create player's hand.
 function createHand() {
-    var hand = [];
+    let hand = [];
     for (i=0; i<5; i++) { 
-        let luck = Math.floor(Math.random() * deck.length); // Numero casuale che individua una carta all'interno del mazzo. / Random number that identifies a card in the deck.
-        hand = hand.concat(deck.splice(luck,1)); // Inserire la carta casuale nella mano dal giocatore e al contempo eliminarla dal mazzo. / Insert the random card into the player's hand and remove it from the deck at the same time.
+        let luck = Math.floor(Math.random() * playDeck.length); // Numero casuale che individua una carta all'interno del mazzo. / Random number that identifies a card in the deck.
+        hand = hand.concat(playDeck.splice(luck,1)); // Inserire la carta casuale nella mano dal giocatore e al contempo eliminarla dal mazzo. / Insert the random card into the player's hand and remove it from the deck at the same time.
     }
+    hand.sort(function (a, b) { return a.card - b.card; }); // Ordinare la mano del giocatore. / Order the player's hand.
     return hand;
 }
 
@@ -41,6 +45,6 @@ function show(nowISeeYou) {
     }).join("<br />");
 }
 
-createDeck();
+playDeck = createDeck();
 playerHand = createHand();
 show(playerHand);
